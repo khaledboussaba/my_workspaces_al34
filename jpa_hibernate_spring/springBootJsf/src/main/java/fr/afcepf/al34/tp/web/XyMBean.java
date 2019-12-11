018fa333b94fdb74b8ec24a76b2d0167db664ae8
unknown <khaledboussaba@gmail.com>
2019-12-11 11:27:01 +0100

    modification tp springBootJsf

--- jpa_hibernate_spring/springBootJsf/src/main/java/fr/afcepf/al34/tp/web/XyMBean.java
+++ jpa_hibernate_spring/springBootJsf/src/main/java/fr/afcepf/al34/tp/web/XyMBean.java
@@ -1,25 +1,42 @@
 package fr.afcepf.al34.tp.web;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 import fr.afcepf.al34.tp.service.XyService;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+//@Component
+//@Named
 @ManagedBean
+
+//@Scope("request")
 @RequestScoped
+
 @Getter @Setter 
 @NoArgsConstructor
 public class XyMBean {
 	
 	private String data;
+	private String s;
 	
+	//@Autowired
+	@Inject
 	private XyService xyService;
 	
+	@PostConstruct
 	public void init() {
-		data="blabla";
+		//data="blabla";
+		data=xyService.getData();
+	}
+	
+	public String doDo() {
+		System.out.println("doDo() , s="+s);
+		return null;
 	}
 	
 	
