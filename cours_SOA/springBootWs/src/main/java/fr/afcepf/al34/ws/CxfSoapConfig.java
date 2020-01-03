259f7bc386030e0922a4a429f68bef40d068b950
unknown <khaledboussaba@gmail.com>
2020-01-03 16:13:21 +0100

    springboot with WebService : new project for interface and dto

--- cours_SOA/springBootWs/src/main/java/fr/afcepf/al34/ws/CxfSoapConfig.java
+++ cours_SOA/springBootWs/src/main/java/fr/afcepf/al34/ws/CxfSoapConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ImportResource;
 import fr.afcepf.al34.ws.service.ICalculTva;
 
 
+
 @Configuration
 @EnableAutoConfiguration
 @ImportResource({ "classpath:META-INF/cxf/cxf.xml" })
