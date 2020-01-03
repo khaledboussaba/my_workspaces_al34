748a33517956d4577cba0b87addbe6c742159dc3
unknown <khaledboussaba@gmail.com>
2020-01-03 10:33:26 +0100

    Soap WebService + connection to another machine

--- cours_SOA/cliSoap/src/main/java/fr/afcepf/al34/client/ClientSoapApp.java
+++ cours_SOA/cliSoap/src/main/java/fr/afcepf/al34/client/ClientSoapApp.java
@@ -1,5 +1,7 @@
 package fr.afcepf.al34.client;
 
+import java.net.URL;
+
 import fr.afcepf.al34.dto.ResCalculTva;
 import fr.afcepf.al34.serveur.CalculTvaImplService;
 import fr.afcepf.al34.serveur.ICalculTva;
@@ -7,14 +9,22 @@ import fr.afcepf.al34.serveur.ICalculTva;
 public class ClientSoapApp {
 
 	public static void main(String[] args) {
-
-		ICalculTva proxyWSTva = (new CalculTvaImplService()).getCalculTvaImplPort();
-		String auteur = proxyWSTva.getAuteur();
-		System.out.println(auteur);
+		try {
+			//URL wsdlUrl = new URL("http://localhost:8080/servEjb/CalculTvaImpl?wsdl");
+			// Le serveur jboss accepte des requetes autres que localhost si l'option -b 0.0.0.0 précisée au lancement du serveur (depuis eclipse ou pas)
+			URL wsdlUrl = new URL("http://192.168.102.183:8080/servEjb/CalculTvaImpl?wsdl");
+			
+			ICalculTva proxyWSTva = (new CalculTvaImplService(wsdlUrl)).getCalculTvaImplPort();
+			String auteur = proxyWSTva.getAuteur();
+			System.out.println(auteur);
+			
+			ResCalculTva res = proxyWSTva.tvaEtTtc(200, 20);
+			System.out.println("tva = " + res.getTva());
+			System.out.println("ttc = " + res.getTtc());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		ResCalculTva res = proxyWSTva.tvaEtTtc(200, 20);
-		System.out.println("tva = " + res.getTva());
-		System.out.println("ttc = " + res.getTtc());
 	}
 
 }
