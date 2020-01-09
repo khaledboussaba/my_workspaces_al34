9819cf16fa36fd6abf26f5d0d2489412aebbd486
unknown <khaledboussaba@gmail.com>
2020-01-09 14:45:24 +0100

    project nodeWebService : deuxieme webService with Nodejs

--- projet_NodeWebService/server.js
+++ projet_NodeWebService/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+// GET http://localhost:8585/essai1
 app.get('/essai1', (req, res, next) => {
   var objData = {
     ref : 'ref1',
@@ -9,9 +10,27 @@ app.get('/essai1', (req, res, next) => {
   res.send(objData);
 });
 
+// GET http://localhost:8585/api-emprunt/mensualite?montant=2000&nbMois=120&tauxAnnuelPct=1
+app.get('/api-emprunt/mensualite', (req, res, next) => {
+  var montant = Number(req.query.montant); // 2000 ou ...
+  var nbMois = Number(req.query.nbMois); // 120 pour 10 ans
+  var tauxAnnuelPct = Number(req.query.tauxAnnuelPct); // 1 pour 1% par an
+  var tauxMensuel = tauxAnnuelPct / 100 / 12;
+  var mensualite = montant * tauxMensuel / (1 - Math.pow(1.0 + tauxMensuel,-nbMois));
+  var objDataRes = {
+    montant : montant,
+    nbMois : nbMois,
+    tauxAnnuelPct : tauxAnnuelPct,
+    mensualite : mensualite
+  };
+  res.send(objDataRes);
+});
+
 app.listen(8585, () => {
   console.log('http://localhost:8585');
 });
 
 // node server.js pour executer
 // http://localhost:8585/essai1
+
+// installer nodeamon pour ne pas restarter node
