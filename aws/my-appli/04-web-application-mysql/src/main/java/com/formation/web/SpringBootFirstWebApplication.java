bb15ae2463a08772b00144dc4c53fb8959a10f8c
unknown <khaledboussaba@gmail.com>
2020-02-21 16:49:07 +0100

    new example springboot to deploy in aws

---
+++ aws/my-appli/04-web-application-mysql/src/main/java/com/formation/web/SpringBootFirstWebApplication.java
@@ -0,0 +1,22 @@
+package com.formation.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.formation.web")
+public class SpringBootFirstWebApplication extends SpringBootServletInitializer { // AWS
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootFirstWebApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+	}
+
+}
